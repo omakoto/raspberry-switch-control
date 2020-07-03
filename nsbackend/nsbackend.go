84b38b7e4abbb07ce18ae450b170ffbf3115605c
Makoto Onuki <makoto.onuki@google.com>
2020-07-02 20:50:12 -0700

    More backend commands

--- nsbackend/nsbackend.go
+++ nsbackend/nsbackend.go
@@ -5,9 +5,14 @@ import (
 	"github.com/mzyy94/nscon"
 	"github.com/omakoto/go-common/src/common"
 	"github.com/pborman/getopt/v2"
+	"math"
 	"os"
 	"strconv"
 	"strings"
+)
+
+const (
+	analogToDigitalThreshold = 1.0
 )
 
 var (
@@ -18,23 +23,41 @@ var (
 func parseCommand(s string) (command string, arg float64, hasArg bool, err error) {
 	command = ""
 	arg = 0
-	hasArg = true
+	hasArg = false
 
 	arr := strings.Fields(s)
 	if len(arr) == 0 {
 		return "", 0, false, nil
 	}
-	command = arr[0]
+	command = strings.ToLower(arr[0])
 	if len(arr) > 1 {
 		arg, err = strconv.ParseFloat(arr[1], 32)
 		if err != nil {
 			common.Warnf("Invalid float: %#v", arr[1])
 			return "", 0, false, err
 		}
+		if arg < -1 {
+			arg = -1
+		} else if arg > 1 {
+			arg = 1
+		}
 		hasArg = true
 	}
-	common.Debugf("Command=%s arg=%d", command, arg)
+	common.Debugf("Command=%#v arg=%f", command, arg)
 	return
+}
+
+func aToD(arg float64, neg, pos *uint8) {
+	if arg >= analogToDigitalThreshold {
+		*pos = 1
+		*neg = 0
+	} else if arg <= -analogToDigitalThreshold {
+		*pos = 0
+		*neg = 1
+	} else {
+		*pos = 0
+		*neg = 0
+	}
 }
 
 func mainLoop(con *nscon.Controller) (err error) {
@@ -48,9 +71,10 @@ func mainLoop(con *nscon.Controller) (err error) {
 
 		// Digital button arg: 0 or 1
 		var darg uint8 = 0
-		if !hasArg || arg != 0 {
+		if !hasArg || math.Abs(arg) >= analogToDigitalThreshold {
 			darg = 1
 		}
+		fdarg := float64(darg)
 
 		switch command {
 		case "a": // A
@@ -72,36 +96,109 @@ func mainLoop(con *nscon.Controller) (err error) {
 		case "+", "p": // plus
 			con.Input.Button.Plus = darg
 
-		case "l1": // L1
+		case "l", "l1": // L1
 			con.Input.Button.L = darg
-		case "l2": // L2
+		case "l2", "lz": // L2
 			con.Input.Button.ZL = darg
-		case "r1": // R1
+		case "r", "r1": // R1
 			con.Input.Button.R = darg
-		case "r2": // R2
+		case "r2", "rz": // R2
 			con.Input.Button.ZR = darg
 
-		case "u": // D-pad up
+		case "pu": // D-pad up
 			con.Input.Dpad.Up = darg
-		case "d": // D-pad down
+		case "pd": // D-pad down
 			con.Input.Dpad.Down = darg
-		case "l": // D-pad left
+		case "pl": // D-pad left
 			con.Input.Dpad.Left = darg
-		case "r": // D-pad right
+		case "pr": // D-pad right
 			con.Input.Dpad.Right = darg
+
+		case "pur", "pru": // D-pad
+			con.Input.Dpad.Up = darg
+			con.Input.Dpad.Right = darg
+		case "pul", "plu": // D-pad
+			con.Input.Dpad.Up = darg
+			con.Input.Dpad.Left = darg
+		case "pdr", "prd": // D-pad
+			con.Input.Dpad.Down = darg
+			con.Input.Dpad.Right = darg
+		case "pdl", "pld": // D-pad
+			con.Input.Dpad.Down = darg
+			con.Input.Dpad.Left = darg
+
+		case "px": // D-pad alternative
+			aToD(arg, &con.Input.Dpad.Right, &con.Input.Dpad.Right)
+		case "py": // D-pad alternative
+			aToD(arg, &con.Input.Dpad.Down, &con.Input.Dpad.Up)
 
 		case "lx": // Left stick X
 			con.Input.Stick.Left.X = arg
 		case "ly": // Left stick Y
 			con.Input.Stick.Left.Y = arg
 
+			// Left stick alternative
+		case "lu":
+			con.Input.Stick.Left.X = 0
+			con.Input.Stick.Left.Y = -fdarg
+		case "ld":
+			con.Input.Stick.Left.X = 0
+			con.Input.Stick.Left.Y = fdarg
+		case "ll":
+			con.Input.Stick.Left.X = -fdarg
+			con.Input.Stick.Left.Y = 0
+		case "lr":
+			con.Input.Stick.Left.X = fdarg
+			con.Input.Stick.Left.Y = 0
+		case "lur", "lru":
+			con.Input.Stick.Left.X = fdarg
+			con.Input.Stick.Left.Y = -fdarg
+		case "lul", "llu":
+			con.Input.Stick.Left.X = -fdarg
+			con.Input.Stick.Left.Y = -fdarg
+		case "ldr", "lrd":
+			con.Input.Stick.Left.X = fdarg
+			con.Input.Stick.Left.Y = fdarg
+		case "ldl", "lld":
+			con.Input.Stick.Left.X = -fdarg
+			con.Input.Stick.Left.Y = fdarg
+
 		case "rx": // Right stick X
 			con.Input.Stick.Right.X = arg
 		case "ry": // Right stick Y
-			con.Input.Stick.Right.X = arg
+			con.Input.Stick.Right.Y = arg
+
+			// Right stick alternative
+		case "ru":
+			con.Input.Stick.Right.X = 0
+			con.Input.Stick.Right.Y = -fdarg
+		case "rd":
+			con.Input.Stick.Right.X = 0
+			con.Input.Stick.Right.Y = fdarg
+		case "rl":
+			con.Input.Stick.Right.X = -fdarg
+			con.Input.Stick.Right.Y = 0
+		case "rr":
+			con.Input.Stick.Right.X = fdarg
+			con.Input.Stick.Right.Y = 0
+		case "rur", "rru":
+			con.Input.Stick.Right.X = fdarg
+			con.Input.Stick.Right.Y = -fdarg
+		case "rul", "rlu":
+			con.Input.Stick.Right.X = -fdarg
+			con.Input.Stick.Right.Y = -fdarg
+		case "rdr", "rrd":
+			con.Input.Stick.Right.X = fdarg
+			con.Input.Stick.Right.Y = fdarg
+		case "rdl", "rld":
+			con.Input.Stick.Right.X = -fdarg
+			con.Input.Stick.Right.Y = fdarg
+
 		default:
 			common.Warnf("Unknown command: %#v\n", command)
+			continue
 		}
+		common.Dump("State:", con.Input)
 	}
 	return nil
 }
